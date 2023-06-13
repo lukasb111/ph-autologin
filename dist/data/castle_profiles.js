7ff60676bc2e40a380d40f708b352efc2957e5f2
Lukáš Brýla <lukybryla9@gmail.com>
2023-06-13 18:42:57 +0200

    production build

--- dist/data/castle_profiles.js
+++ dist/data/castle_profiles.js
@@ -1,12 +1,1 @@
-export default{
-    "1": [
-        {"div_defender": "Obránce (vhodný pro nováčka)"},
-        {"div_economist": "Hospodář (vhodný pro nováčka)"},
-        {"div_farmer": "Farmář (pro hráče se 100 dny či VIP účtem)"},
-        {"div_attacker": "Útočník (pro hráče s 200 dny či VIP účtem)"},
-        {"div_storage": "Skladník (pro hráče s 300 dny či VIP účtem)"}
-    ],
-    "5": [
-        {"div_bratrstvo1": "Hrad Černého Bratrstva"}
-    ]
-}
+export default{1:[{div_defender:"Obránce (vhodný pro nováčka)"},{div_economist:"Hospodář (vhodný pro nováčka)"},{div_farmer:"Farmář (pro hráče se 100 dny či VIP účtem)"},{div_attacker:"Útočník (pro hráče s 200 dny či VIP účtem)"},{div_storage:"Skladník (pro hráče s 300 dny či VIP účtem)"}],5:[{div_bratrstvo1:"Hrad Černého Bratrstva"}]};
